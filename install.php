8186e65833e353840b9a90b2fadac45189b06552
Martin Hecht <mrbaseman@gmx.de>
2016-12-21 15:38:00 +0100

    initial import of v4_9_10

---
+++ install.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ *
+ * @category        modules
+ * @package         show_menu2
+ * @author          Independend-Software-Team
+ * @author          WebsiteBaker Project
+ * @copyright       2004-2009, Ryan Djurovich
+ * @copyright       2009-2011, Website Baker Org. e.V.
+ * @link            http://www.websitebaker2.org/
+ * @license         http://www.gnu.org/licenses/gpl.html
+ * @platform        WebsiteBaker 2.8.x
+ * @requirements    PHP 5.2.2 and higher
+ * @version         $Id: install.php 2114 2014-12-01 10:22:07Z darkviper $
+ * @filesource      $HeadURL: svn://isteam.dynxs.de/wb_svn/wb280/branches/2.8.x/wb/modules/ShowMenu2/install.php $
+ * @lastmodified    $Date: 2014-12-01 11:22:07 +0100 (Mo, 01. Dez 2014) $
+ *
+ */
+
+// Must include code to stop this file being access directly
+if(defined('WB_PATH') == false) { die("Cannot access this file directly"); }
